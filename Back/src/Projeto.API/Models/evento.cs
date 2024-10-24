47cc2fb58d7d6928cd88a0299e6b1839f4000e21
washington <lepexgamer102000@gmail.com>
2024-10-24 09:59:51 -0300

    Commit de teste

--- Back/src/Projeto.API/Models/evento.cs
+++ Back/src/Projeto.API/Models/evento.cs
@@ -8,7 +8,6 @@ namespace Projeto.API.Models
 {
     public class Evento
     {
-    public int MyProperty { get; set; }
     public int EventoId { get; set; }
     public string? Local { get; set; }
     public string? DataEvento { get; set; }
