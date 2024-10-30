8891bee86f5575fdbe9fbe1ec5b1bc9731eb6c6c
Washington <lepexgamer102000@gmail.com>
2024-10-29 22:07:44 -0300

    DbContext

--- Back/src/Projeto.API/Data/DataContext.cs
+++ Back/src/Projeto.API/Data/DataContext.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto.API.Models;
 
 namespace Projeto.API.Data
 {
     public class DataContext : DbContext
     {
-
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Evento>? Eventos { get; set; }
     }
 }
